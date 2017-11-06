b0602ae1c067b9025388e4cef4e76513c4df297d
Your Name <you@example.com>
2017-11-06 15:47:49 -0500

    final reinforcement-programing exercise nov3rd

--- reinforcement-exercise1.rb
+++ reinforcement-exercise1.rb
@@ -10,7 +10,7 @@
 
 # # 
 
-def places_to_go(accesibility, patrons)
+def places_to_go
 	venues = [
 		{ address: "123 Main Street", city: "Toronto", wheelchair_accessible: true, capacity: 100 },
 		{ address: "567 Centre Street", city: "Toronto", wheelchair_accessible: false, capacity: 400 },
@@ -22,17 +22,27 @@ def places_to_go(accesibility, patrons)
 		{ address: "10 Spruce Avenue Ouest", city: "Montreal", wheelchair_accessible: false, capacity: 525 }
 	]
 	
-		venues.each do |venue|  
-			
-        accessibility = :wheelchair_accessible
-	    patrons = :capacity
-			if patrons <= 150 && accessibility == true
-			return [accessibility, patrons]
-		end
-	end
-
 		
-end
+locations = []
+		venues.each do |value|
+		if value[:city] == "Toronto" && value[:wheelchair_accessible] == true && value[:capacity] >= 140
+			locations << value
+			# puts locations
+		end
 
-ß
-puts places_to_go(true, 10 )
+	end
+	locations
+end
+ puts places_to_go
+
+
+#puts places_to_go(true, 10 )
+	# 	venues.each do |venue|  
+			
+ #        accessibility = :wheelchair_accessible
+	#     patrons = :capacity
+	# 		if patrons <= 150 && accessibility == true
+	# 		return [accessibility, patrons]
+	# 	end
+	# end
+
